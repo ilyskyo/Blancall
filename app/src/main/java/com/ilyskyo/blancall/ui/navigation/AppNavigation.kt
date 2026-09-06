af63d467924b4d0f180b57bd5d28b5739556b723
ilyskyo <ilyskyo@users.noreply.github.com>
2026-09-06 18:02:29 +0800

    feat: 自定义挖空 v2 - 模式选择全入口自定义项、编辑器三模式切换实时预览、页面框架对齐规范

--- app/src/main/java/com/ilyskyo/blancall/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/ilyskyo/blancall/ui/navigation/AppNavigation.kt
@@ -266,7 +266,7 @@ fun AppNavigation() {
         }
 
         composable(
-            route = "practice/{articleId}?mode={mode}&resume={resume}&sectionMode={sectionMode}",
+            route = "practice/{articleId}?mode={mode}&resume={resume}&sectionMode={sectionMode}&custom={custom}",
             arguments = listOf(
                 navArgument("articleId") { type = NavType.LongType },
                 navArgument("mode") {
@@ -280,6 +280,10 @@ fun AppNavigation() {
                 navArgument("sectionMode") {
                     type = NavType.StringType
                     defaultValue = ""
+                },
+                navArgument("custom") {
+                    type = NavType.StringType
+                    defaultValue = "false"
                 }
             ),
             enterTransition = enterSlide,
@@ -296,7 +300,8 @@ fun AppNavigation() {
             PracticeScreen(
                 navController, listOf(articleId), initialMode,
                 resume = resumeStr == "true",
-                initialSectionMode = initialSectionMode
+                initialSectionMode = initialSectionMode,
+                openCustomPicker = backStackEntry.arguments?.getString("custom") == "true"
             )
         }
 
