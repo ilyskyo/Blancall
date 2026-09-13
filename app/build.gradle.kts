8a9cc170ffedbd04697f28aeb75f498ecb8e720a
ilyskyo <205641139+ilyskyo@users.noreply.github.com>
2026-09-13 17:10:58 +0800

    release: 6.9 (versionCode 31)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ilyskyo.blancall"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "6.8"
+        versionCode = 31
+        versionName = "6.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
