1137f87a27e40590ce449959498904185a0c54e0
ilyskyo <205641139+ilyskyo@users.noreply.github.com>
2026-09-20 23:39:40 +0800

    release: 7.0.1 (versionCode 33)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ilyskyo.blancall"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "7.0"
+        versionCode = 33
+        versionName = "7.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
